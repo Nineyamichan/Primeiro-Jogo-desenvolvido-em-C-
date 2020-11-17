d81d62fc8d4913fccd3a1b7313ea46c95eeae279
Aline Alves <50326575+Nineyamichan@users.noreply.github.com>
2020-11-17 10:06:02 -0300

    Add files via upload

---
+++ Botoes.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Botoes : MonoBehaviour
+{
+    Rigidbody rb2D;
+    Animator playerAnim;
+
+    public float Velocity, Speed;
+
+    void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody>();
+        playerAnim = gameObject.GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        Move(Speed);
+    }
+
+    void Move(float _speed)
+    {
+        rb2D.transform.Translate(_speed * Time.deltaTime, 0, 0);
+
+     
+    }
+   
+    public void MoveRight()
+    {
+        Speed = Velocity;
+    }
+    public void MoveLeft()
+    {
+        Speed = -Velocity;
+    }
+    
+    public void Jump()
+    {
+        rb2D.AddForce(Vector2.up * 4, ForceMode.Impulse);
+    }
+}
